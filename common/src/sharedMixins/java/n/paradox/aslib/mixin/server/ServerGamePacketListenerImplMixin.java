e62c50e0e016a1c463707a55be3a295ae618b24d
akaize <sahsa61842@gmail.com>
2026-06-12 13:13:34 +0300

    0-0

--- common/src/sharedMixins/java/n/paradox/aslib/mixin/server/ServerGamePacketListenerImplMixin.java
+++ common/src/sharedMixins/java/n/paradox/aslib/mixin/server/ServerGamePacketListenerImplMixin.java
@@ -8,12 +8,15 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin({ServerGamePacketListenerImpl.class})
 public class ServerGamePacketListenerImplMixin {
+    @Shadow
+    public ServerPlayer player;
     @Inject(
             method = {"handleCustomPayload"},
             at = {@At("HEAD")},
@@ -24,7 +27,7 @@ public class ServerGamePacketListenerImplMixin {
         if (AsLibNetworking.REGISTRY.isPresent(id)) {
             FriendlyByteBuf buf = packet.getData();
             Packet decodedPacket = AsLibNetworking.REGISTRY.get(id).decoder().decode(buf);
-            ServerPlayer sender = ((ServerGamePacketListenerImpl)this).player;
+            ServerPlayer sender = this.player;
 
             try {
                 AsLibNetworking.HANDLER.handle(decodedPacket, sender);
