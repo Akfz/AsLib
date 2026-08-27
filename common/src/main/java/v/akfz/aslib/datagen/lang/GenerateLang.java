230d49c2c6b6211974aedaae4b9c2859155bd773
akaize <sahsa61842@gmail.com>
2026-08-27 15:45:29 +0300

    rewrited

--- common/src/main/java/v/akfz/aslib/datagen/lang/GenerateLang.java
+++ common/src/main/java/v/akfz/aslib/datagen/lang/GenerateLang.java
@@ -1,31 +1,10 @@
 package v.akfz.aslib.datagen.lang;
 
 import v.akfz.aslib.datagen.api.DataProvider;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Data generator for .lang files
- *
- * Example usage:
- * <pre>
- * new GenerateLang()
- *     .addLang(new LangData("mymod", "en_us")
- *         .addEntity("testentity", "Test Entity")
- *         .addItem("testitem", "Test Item")
- *         .addBlock("testblock", "Test Block")
- *         .addCreativeTab("main", "My Mod")
- *         .add("key.custom", "Custom Translation"))
- *     .addLang(new LangData("mymod", "ru_ru")
- *         .addEntity("testentity", "Тестовая сущность")
- *         .addItem("testitem", "Тестовый предмет")
- *         .addBlock("testblock", "Тестовый блок")
- *         .addCreativeTab("main", "Мой мод"))
- *     .run("common");
- * </pre>
- */
 public class GenerateLang extends DataProvider {
 
 	private final List<LangData> langDataList = new ArrayList<>();
@@ -34,12 +13,16 @@ public class GenerateLang extends DataProvider {
 	}
 
 	public GenerateLang addLang(LangData langData) {
-		this.langDataList.add(langData);
+		if (langData != null) {
+			this.langDataList.add(langData);
+		}
 		return this;
 	}
 
 	public GenerateLang addLangs(LangData... langDataArray) {
-		this.langDataList.addAll(Arrays.asList(langDataArray));
+		if (langDataArray != null) {
+			this.langDataList.addAll(Arrays.asList(langDataArray));
+		}
 		return this;
 	}
 
